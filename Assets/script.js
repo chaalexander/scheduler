c9417a8e332b38a546d8a511eeca721d81693f72
charlenne alexander <charlennep@gmail.com>
2020-03-08 18:22:41 -0500

    blocks are showing on the screen

--- Assets/script.js
+++ Assets/script.js
@@ -1,43 +1,41 @@
-// variables
-
-var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
-console.log(hours);
-
-var showtime = $("#currentDay").text(setTime);
-console.log(showtime);
-
-// creating functons
-// time
-function setTime() {
-  var time = moment().format("MMMM Do YYYY, h:mm a");
-  return time;
-}
-console.log(setTime());
-
-// calendar
-function createCalendar() {
-  var newDiv = $("<div>");
-  console.log(newDiv);
-  newDiv.append("#calendarHours");
-
-  var newForm = $("<form>");
-  console.log(newForm);
-  newForm.append("#newDiv");
-
-  var btn = $("<button>").text("save");
-  console.log(btn);
-  btn.append(guestInput);
-
-  for (var i = 0; i < hours.length; i++) {
-    var guestInput = $("<input>");
-    console.log(guestInput);
-    guestInput.append(newForm);
-  }
-}
-// $(".container").html(createCalendar());
-// console.log(createCalendar());
-
 $(document).ready(function() {
-  setTime();
-  createCalendar();
+  // variables
+
+  var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+  console.log(hours);
+
+  var showtime = $("#currentDay").text(setTime);
+  console.log(showtime);
+
+  // creating functons
+  // time
+  function setTime() {
+    var time = moment().format("MMMM Do YYYY, h:mm a");
+    return time;
+  }
+  console.log(setTime());
+
+  // calendar
+  function createCalendar() {
+    var newDiv = $("<div>");
+    console.log(newDiv);
+    $("#container").append(newDiv);
+
+    var newForm = $("<form>");
+    console.log(newForm);
+    $(newDiv).append(newForm);
+
+    for (var i = 0; i < hours.length; i++) {
+      var guestInput = $("<input type= text class= guest>");
+      console.log(guestInput);
+      $(newForm).append(guestInput);
+
+      var btn = $("<button class= saveBtn>").text("save");
+      console.log(btn);
+      $(btn).attr("data-index", i);
+      $(guestInput).append(btn);
+    }
+  }
+
+  $("#container").html(createCalendar);
 });
