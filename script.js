e99352c0f629cf7d1bf772406b90fe51b44294cc
charlenne alexander <charlennep@gmail.com>
2020-03-08 14:25:01 -0500

    time is showing

--- script.js
+++ script.js
@@ -3,12 +3,24 @@
 var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 console.log(hours);
 
-// create time
+var showtime = $("#currentDay").text(setTime);
+console.log(showtime);
+
+// creating functons
+// time
+function setTime() {
+  var time = moment().format("MMMM Do YYYY, h:mm a");
+  return time;
+}
+console.log(setTime());
+
+// calendar
+function createCalendar() {
+  newRow = $("<try>").append(".container");
+  newBox = $("<input>").append("<tr>");
+}
+createCalendar();
 
 // $(document).ready(function() {
-
-//   $("save").on("click", function() {});
-
-//   console.log("save");
 
 // });
