1333cb8f4b43caccba34e839ed2617636616b14f
charlenne alexander <charlennep@gmail.com>
2020-03-09 19:11:48 -0500

    collor blocks working. time fixed. missing localStorage

--- Assets/script.js
+++ Assets/script.js
@@ -26,24 +26,42 @@ console.log(setTime());
 // calendar
 function createCalendar() {
   for (var i = 0; i < hours.length; i++) {
-    var guestInput = $(
-      "<input type= 'text' placeholder= 'What is up!' class= guest>"
-    );
-    console.log(guestInput);
-    $(newForm).append(guestInput);
+    var colorTime;
+    if (moment().hour() === hours[i]) {
+      colorTime = "present";
+    } else if (moment().hour() < hours[i]) {
+      colorTime = "future";
+    } else if (moment().hour() > hours[i]) {
+      colorTime = "past";
+    }
+    var guestInput = $(`<div class="input-group mb-3 ${colorTime}"><div class="input-group-prepend">
+    <span class="input-group-text">${hours[i]}:00</span>
+      </div>
+      <input type="text" class="form-control ${colorTime}">
+      <div class="input-group-append">
+        <span class="input-group-text"><button class= saveBtn><i class= 'fas fa-save'></i></span>
+      </div>
+    </div>`);
+    $("#container").append(guestInput);
 
-    var btn = $("<button class= saveBtn><i class= 'fas fa-save'></i>");
-    console.log(btn);
-    $(guestInput).after(btn);
+    // var guestInput = $(
+    //   "<input type= 'text' placeholder= 'What is up!' class= guest>"
+    // );
+    // console.log(guestInput);
+    // $(newForm).append(guestInput);
+
+    // var btn = $("<button class= saveBtn><i class= 'fas fa-save'></i>");
+    // console.log(btn);
+    // $(guestInput).after(btn);
   }
 
-  $(".saveBtn").on("click", function() {
-    console.log("you click me ");
-    event.preventDefault();
-    localStorage.setItem("calendar", JSON.stringify(savedArray));
-    localStorage.getItem("calendar");
-    $(".guest").text(savedArray);
-  });
+  // $(".saveBtn").on("click", function() {
+  //   console.log("you click me ");
+  //   event.preventDefault();
+  //   localStorage.setItem("calendar", JSON.stringify(savedArray));
+  //   localStorage.getItem("calendar");
+  //   $(".guest").text(savedArray);
+  // });
 }
 
 // $("#container").html(createCalendar);
